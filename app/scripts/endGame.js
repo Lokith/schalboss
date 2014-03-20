fffb40d594006f3d47862b85978d6734e428cefc
GOUNOT Aurelien <aurelien.gounot@sncf.fr>
2014-03-20 11:31:43 +0100

    [endGame] Ajout du titre Scores

--- app/scripts/endGame.js
+++ app/scripts/endGame.js
@@ -2,3 +2,12 @@
  * Created by codfractal on 19/03/14.
  */
 
+function showLeaderboard() {
+
+    game.add.image(0,0,'fondecran_start');
+    var fontTitre = game.add.retroFont('font', 31, 25, Phaser.RetroFont.TEXT_SET6, 10, 1, 1);
+    fontTitre.text = 'SCORES';
+    var titre = game.add.image(180, 40, fontTitre);
+    titre.scale.setTo(1.4,1.4);
+
+}
